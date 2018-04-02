10ee9febc177629db795eef5d54d62cfc1c980a1
BobYeh <bobgary2002@gmail.com>
2018-04-02 23:19:41 +0900

    Add Title scene

---
+++ Assets/Scripts/0_Title/TitleSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject gameGuideView;
+	
+    public void OnClickedGameGuideButton()
+    {
+        gameGuideView.SetActive(true);
+    }
+
+
+    public void OnClickedGameStartButton()
+    {
+        SceneManager.LoadSceneAsync(SceneName.GameScene);
+    }
+}
---
+++ Assets/Scripts/Common/SceneName.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneName
+{
+    public const string TitleScene = "Scene/TitleScene";
+    public const string GameScene = "Scene/GameScene";
+}
